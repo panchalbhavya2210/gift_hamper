7027b507aaa9facc4487e65c7efb6c1159a36953
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2024-01-09 20:36:55 +0530

    k

--- giftapp/urls.py
+++ giftapp/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.viewPage, name="index")
+    path("", views.viewPage, name="index"),
+    path("about", views.aboutPage, name="about"),
+    path("cart", views.cartPage, name="cart"),
+    path("checkout", views.checkoutPage, name="checkout"),
+    path("contact", views.contactPage, name="contact"),
+    path("product-details", views.prodDetails, name="product-details"),
+    path("shop", views.shopPage, name="shop"),
+    path("under-construction", views.underConsPage, name="under-construction"),
+    path("wishlist", views.wishList, name="wishlist"),
 ]
--- giftapp/views.py
+++ giftapp/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def viewPage(request):
     return render(request, "index.html")
+def aboutPage(request):
+    return render(request, "about.html")
+def blogDetails(request):
+    return render(request, "blog-details.html")
+def cartPage(request):
+    return render(request, "cart.html")
+def checkoutPage(request):
+    return render(request, "checkout.html")
+def contactPage(request):
+    return render(request, "contact.html")
+def prodDetails(request):
+    return render(request, "product-details.html")
+def shopPage(request):
+    return render(request, "shop.html")
+def underConsPage(request):
+    return render(request, "under-construction.html")
+def wishList(request):
+    return render(request, "wishlist.html")
