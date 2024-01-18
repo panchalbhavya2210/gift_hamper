92b67f92b6367259260532aa853833baa0bc590e
sonukshatriya <sonukshatriya2017@gmail.com>
2024-01-18 21:32:22 +0530

    done

--- giftapp/urls.py
+++ giftapp/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     path('insert', views.registerUser, name='insert'),
     path("signup", views.signUp, name="signup"),
     path("login", views.login, name="login"),
-<<<<<<< HEAD
-=======
     path("register_seller", views.signUpSeller, name="register_seller"),
->>>>>>> ef7104768ecb3a4a80e68528499ce5a823a61904
     path("cart", views.cartPage, name="cart"),
     path("checkout", views.checkoutPage, name="checkout"),
     path("contact", views.contactPage, name="contact"),
